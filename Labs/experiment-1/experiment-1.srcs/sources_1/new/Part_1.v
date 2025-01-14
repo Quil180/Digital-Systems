3541afc965a45b18b5417e5c5c194fd18841829f
Quil180 <quil180gaming@gmail.com>
2025-01-14 09:34:38 -0500

    First Commit

---
+++ Labs/experiment-1/experiment-1.srcs/sources_1/new/Part_1.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 01/14/2025 09:33:57 AM
+// Design Name: 
+// Module Name: Part_1
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module Part_1(
+
+    );
+endmodule
